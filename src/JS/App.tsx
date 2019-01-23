ce1e2a07dae0e334753b971246ad15b7b8e9e330
Thomas McAuliffe <tjamesmac@gmail.com>
2019-01-23 18:31:43 +0000

    Remove create react app stuff

--- src/JS/App.tsx
+++ src/JS/App.tsx
@@ -4,22 +4,15 @@ import '../CSS/App.css';
 
 class App extends Component {
   render() {
+
+    // const mapMaker = async () => {
+    //     let response = await fetch();
+    //     let user = await response.json();
+    //     console.log(user);
+    // }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
       </div>
     );
   }
